229f5f2c832fd8a3ffbe9e9d7a937d9c5edc3d42
Oded Padon <oded.padon@gmail.com>
2022-11-28 23:49:56 -0800

    ADTs: implemented a form of MBQI. The ADT solver already chokes for the simple consensus example.

--- experimental/algebraic_data_types_consensus.py
+++ experimental/algebraic_data_types_consensus.py
@@ -41,6 +41,25 @@ forall_assertions = [
     z3.ForAll([n], z3.Implies(member(n,q2), vote(n,v2))),
 ]
 
+def minimize_model(s, n=1):
+    def limit(k):
+        xs = [z3.Const(f'x{i}', S) for i in range(k)]
+        y = z3.Const('y', S)
+        s.add(z3.Exists(xs, z3.ForAll([y], z3.Or(*(y == x for x in xs)))))
+    assert s.check() == z3.sat
+    while True:
+        s.push()
+        limit(n)
+        if (res := s.check()) == z3.sat:
+            m = s.model()
+            s.pop()
+            return n, m
+        else:
+            assert res == z3.unsat
+            s.pop()
+            n += 1
+
+
 print('Fully quantified query:')
 s = z3.Solver()
 s.add(*chain(qf_assertions, forall_assertions))
@@ -73,8 +92,8 @@ for i in range(len(all_live_terms) + 1):
     res = s.check()
     print(res)
     if res == z3.sat:
-        m = s.model()
-        print(m)
+        n_m, m = minimize_model(s)
+        print(n_m, m)
         models.append(m)
     print()
 
@@ -120,22 +139,14 @@ def adt_to_term(t):
         assert False, t
 
 t = z3.Const('t', GroundTerm)
-# n_terms_for_instantiation = 1
-n_terms_for_instantiation = 5
+#n_terms_for_instantiation = 1
+n_terms_for_instantiation = 2
 terms_for_instantiation = [z3.Const(f't_{i}', GroundTerm) for i in range(n_terms_for_instantiation)]
 
-UU = []
-EE = []
-II = []
-BB = []
-VV = []
-WW = []
-for I, M in enumerate(models):
+def add_model(I, M):
     # U = sorted(M.get_universe(S), key=str)
     U = sorted(M.get_universe(S), key=lambda x: int(str(x).split('!')[-1]))
-    UU.append(U)
     E, ES = z3.EnumSort(f'E_{I}', [f'E_{I}_{i}' for i in range(len(U))], ctx=ctx)
-    EE.append((E, ES))
 
     # Ground term ADT that interprets to E:
     functions = [intersection, quorum_of_decided]
@@ -148,7 +159,6 @@ for I, M in enumerate(models):
     constants = [v1, v2, q2]
     constant_interps = [U.index(M.eval(c, model_completion=True)) for c in constants]
     interpret = z3.RecFunction(f'interpret_{I}', GroundTerm, E)
-    II.append(interpret)
     entries = []
     for c, ci in zip(constants, constant_interps):
         entries.append((
@@ -164,11 +174,16 @@ for I, M in enumerate(models):
                 )),
                 ES[fi[xs]]
             ))
+    # def ite_from_entries(es):
+    #     if len(es) == 0:
+    #         return ES[0]
+    #     else:
+    #         return z3.If(es[0][0], es[0][1], ite_from_entries(es[1:]))
     def ite_from_entries(es):
-        if len(es) == 0:
-            return ES[0]
-        else:
-            return z3.If(es[0][0], es[0][1], ite_from_entries(es[1:]))
+        ite = ES[0]
+        for e in reversed(es):
+            ite = z3.If(e[0], e[1], ite)
+        return ite
     ite = ite_from_entries(entries)
     z3.RecAddDefinition(interpret, t, ite)
 
@@ -177,14 +192,11 @@ for I, M in enumerate(models):
         z3.Function(f'body_{I}_{i}', *([E] * f.num_vars() + [z3.BoolSort(ctx=ctx)]))
         for i, f in enumerate(forall_assertions)
     ]
-    BB.append(bodies)
     indicators = [z3.Bool(f'violate_{I}_{i}', ctx=ctx) for i in range(len(forall_assertions))]
-    VV.append(indicators)
     witnesses = [
         [z3.Const(f'witness_{I}_{i}_{j}', E) for j in range(f.num_vars())]
         for i, f in enumerate(forall_assertions)
     ]
-    WW.append(witnesses)
     s.add(z3.Or(*indicators))
     for f, b in zip(forall_assertions, bodies):
         n = f.num_vars()
@@ -199,13 +211,19 @@ for I, M in enumerate(models):
         s.add(z3.Implies(indicators[i], z3.Not(bodies[i](*witnesses[i]))))
         for w in witnesses[i]:
             s.add(z3.Implies(indicators[i], z3.Or(*(w == interpret(t) for t in terms_for_instantiation))))
+    return (M, U, (E, ES), interpret, bodies, indicators, witnesses)
+
+MUEIBVWs = []
+for I, M in enumerate(models):
+    MUEIBVWs.append(add_model(I, M))
+
 # open('test.smt2', 'w').write(s.to_smt2())  # the definition of interpret seems to be missing
 res = s.check()
 print(s.check())
 ground_instantiations = []
 if res == z3.sat:
-    m = s.model()    
-    for (I, M), U, (E, ES), interpret, bodies, indicators, witnesses in zip(enumerate(models), UU, EE, II, BB, VV, WW):
+    m = s.model()
+    for (I, (M, U, (E, ES), interpret, bodies, indicators, witnesses)) in enumerate(MUEIBVWs):
         print(f'model {I}:')
         violated = [m[v] is not None and to_bool(m[v]) for v in indicators]
         assert any(violated)
@@ -239,3 +257,61 @@ print(f'\nThey correspond to terms:', [adt_to_term(m[t]) for t in terms_for_inst
 print(f'\nAll models are hit using the following {len(ground_instantiations)} ground instantiations:')
 for g in ground_instantiations:
     print(f'\n{g}')
+
+
+print(f'\nTrying to do MBQI:\n')
+s_smt = z3.Solver()
+s_smt.add(*qf_assertions)
+while (res := s_smt.check()) != z3.unsat:
+    assert res == z3.sat
+    n_M, M = minimize_model(s_smt)
+    print(f'\nFound {len(MUEIBVWs) + 1}-th model with {n_M} elements: \n{M}')
+    MUEIBVWs.append(add_model(len(MUEIBVWs), M))
+
+    res = s.check()
+    print(f'finding new instantiations: {res}')
+    ground_instantiations = []
+    if res == z3.sat:
+        m = s.model()
+        for (I, (M, U, (E, ES), interpret, bodies, indicators, witnesses)) in enumerate(MUEIBVWs):
+            print(f'model {I}:')
+            violated = [m[v] is not None and to_bool(m[v]) for v in indicators]
+            assert any(violated)
+            assert violated.count(True) == 1
+            for i, v in enumerate(violated):
+                if v:
+                    print(f'    violates assertion {i}: {forall_assertions[i]}')
+                    ws = [m[w] for w in witnesses[i]]
+                    print(f'    witnesses are: {ws}')
+                    adts = []
+                    for w in ws:
+                        eqs = [to_bool(z3.simplify(w == m.eval(interpret(m[t])))) for t in terms_for_instantiation]
+                        assert any(eqs)
+                        j = eqs.index(True)
+                        adts.append(m[terms_for_instantiation[j]])
+                    print(f'    ground term ADTs are: {adts}')
+                    ts = [adt_to_term(t) for t in adts]
+                    print(f'    ground terms are: {ts}')
+                    elems = [M.eval(t) for t in ts]
+                    print(f'    ground terms evaluate to: {elems}')
+                    assert [U.index(e) for e in elems] == [ES.index(m[w]) for w in witnesses[i]]
+                    g = z3.substitute_vars(forall_assertions[i].body(), *ts)
+                    print(f'    ground instance is: {g}')
+                    print(f'    it evaluates to: {M.eval(g)}')
+                    assert z3.is_false(M.eval(g)), (I, ws, adts, ts, elems, g, M.eval(g))
+                    if len(ground_instantiations) == I:  # append only the first violated in each model
+                        ground_instantiations.append(g)
+    elif res == z3.unsat:
+        print(f'Cannot hit all models with {n_terms_for_instantiation} ground terms')
+        break
+    else:
+        assert False
+
+    print(f'\nSummary for the {I}-th iteration:')
+    print(f'\nFound the following ADTs:', [m[t] for t in terms_for_instantiation])
+    print(f'\nThey correspond to terms:', [adt_to_term(m[t]) for t in terms_for_instantiation])
+    print(f'\nAll models are hit using the following {len(ground_instantiations)} ground instantiations:')
+    for g in ground_instantiations:
+        print(f'\n{g}')
+        s_smt.add(g)
+print(f'ADT-based MBQI done! (s_smt is {s_smt.check()})')
