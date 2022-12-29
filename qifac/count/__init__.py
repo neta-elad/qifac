eb4762ae35c8194c00e10b0ea84200a471411685
Neta Elad <elad.neta@gmail.com>
2022-12-29 18:40:10 +0200

    counting terms and symbols

--- qifac/count/__init__.py
+++ qifac/count/__init__.py
@@ -1,7 +1,74 @@
 import io
-from typing import Any, TextIO
+from typing import Any, Dict, Set, TextIO, Tuple
 
 import z3
+
+
+def count_symbols(smt_file: TextIO, show: bool = False) -> None:
+    symbols: Dict[Tuple[int, z3.SortRef], Set[z3.FuncDeclRef]] = {}
+
+    def walk_tree(formula: z3.ExprRef) -> None:
+        if z3.is_app(formula) and formula.decl().kind() == z3.Z3_OP_UNINTERPRETED:
+            decl = formula.decl()
+            key = decl.arity(), decl.range()
+            symbols.setdefault(key, set())
+            symbols[key].add(decl)
+
+        for child in formula.children():
+            walk_tree(child)
+
+    solver = z3.Solver()
+    solver.from_string(smt_file.read())
+
+    for assertion in solver.assertions():
+        walk_tree(assertion)
+
+    keys = list(symbols.keys())
+
+    sorted_keys = sorted(keys, key=lambda key: key[0])
+
+    for key in sorted_keys:
+        value = symbols[key]
+        if show:
+            print(f"{key} => {value}")
+        else:
+            print(f"{key} => {len(value)}")
+
+
+def count_terms(smt_file: TextIO, show: bool = False) -> None:
+    terms: Dict[z3.SortRef, Set[z3.ExprRef]] = {}
+
+    def is_ground(formula: z3.ExprRef) -> bool:
+        if z3.is_var(formula):
+            return False
+        elif z3.is_const(formula):
+            return True
+        else:
+            return all(is_ground(child) for child in formula.children())
+
+    def walk_tree(formula: z3.ExprRef) -> None:
+        if z3.is_app(formula) and formula.decl().kind() == z3.Z3_OP_UNINTERPRETED:
+            decl = formula.decl()
+            key = decl.range()
+            if key != z3.BoolSort() and is_ground(formula):
+                terms.setdefault(key, set())
+                terms[key].add(formula)
+
+        for child in formula.children():
+            walk_tree(child)
+
+    solver = z3.Solver()
+    solver.from_string(smt_file.read())
+
+    for assertion in solver.assertions():
+        walk_tree(assertion)
+
+
+    for sort, sort_terms in terms.items():
+        if show:
+            print(f"{sort} => {sort_terms}")
+        else:
+            print(f"{sort} => {len(sort_terms)}")
 
 
 def count_quantifiers(smt_file: TextIO) -> TextIO:
@@ -9,7 +76,7 @@ def count_quantifiers(smt_file: TextIO) -> TextIO:
     solver = z3.Solver()
     solver.from_string(smt_file.read())
 
-    def count(formula: z3.ExprRef, negations: int) -> Any:
+    def walk_tree(formula: z3.ExprRef, negations: int) -> Any:
         if z3.is_eq(formula):
             return []
         elif z3.is_distinct(formula):
@@ -17,13 +84,16 @@ def count_quantifiers(smt_file: TextIO) -> TextIO:
         elif z3.is_app(formula) and formula.decl().range() != z3.BoolSort():
             return []
         elif z3.is_and(formula) or z3.is_or(formula):
-            results = [count(child, negations) for child in formula.children()]
+            results = [walk_tree(child, negations) for child in formula.children()]
             return results
         elif z3.is_not(formula):
-            return count(formula.children()[0], negations + 1)
+            return walk_tree(formula.children()[0], negations + 1)
         elif z3.is_implies(formula):
             antecedent, consequent = formula.children()
-            return [count(antecedent, negations + 1), count(consequent, negations)]
+            return [
+                walk_tree(antecedent, negations + 1),
+                walk_tree(consequent, negations),
+            ]
         elif z3.is_quantifier(formula):
             head = None
             if formula.is_forall() and negations % 2 == 0:
@@ -32,15 +102,16 @@ def count_quantifiers(smt_file: TextIO) -> TextIO:
                 head = formula.num_vars()
 
             if head is None:
-                return count(formula.children()[0], negations)
+                return walk_tree(formula.children()[0], negations)
             else:
-                return head, count(formula.children()[0], negations)
+                return head, walk_tree(formula.children()[0], negations)
         else:
             return []
 
     for assertion in solver.assertions():
-        result = count(assertion, 0)
-        buffer.write(result)
+
+        result = walk_tree(assertion, 0)
+        buffer.write(str(result))
         buffer.write("\n")
 
     buffer.seek(0)
--- qifac/count/cli.py
+++ qifac/count/cli.py
@@ -1,7 +1,10 @@
+import sys
+from typing import TextIO
+
 import click
 
 from ..utils import smt_file_read_write
-from . import count_quantifiers
+from . import count_quantifiers, count_symbols, count_terms
 
 
 @click.group
@@ -10,3 +13,17 @@ def count() -> None:
 
 
 smt_file_read_write(count, count_quantifiers, "quantifiers")
+
+
+@count.command("symbols")
+@click.argument("smt_file", type=click.File("r"), default=sys.stdin)
+@click.option('--show', '-s', is_flag=True, default=False)
+def wrap_symbols(smt_file: TextIO, show: bool) -> None:
+    count_symbols(smt_file, show)
+
+
+@count.command("terms")
+@click.argument("smt_file", type=click.File("r"), default=sys.stdin)
+@click.option('--show', '-s', is_flag=True, default=False)
+def wrap_terms(smt_file: TextIO, show: bool) -> None:
+    count_terms(smt_file, show)
